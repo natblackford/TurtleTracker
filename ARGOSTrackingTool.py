3829a41232bfc01b3105dc7a543602d06a5b520a
natblackford <natsblackford@gmail.com>
2023-10-02 12:26:05 -0400

    reading in an inividual line

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,3 +8,21 @@
 # Date: 10/2/2023
 #------------------------------------------------------------------------------------
 
+#Parse Data
+lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+#print(lineString[0])
+
+lineData = lineString.split(    )
+#print(lineData[0])
+#print(lineData)
+#assigning variables to objects in the list
+
+ob_id = lineData[0]
+ob_date = lineData[2]
+ob_lc= lineData[3]
+ob_lat = lineData[5]
+ob_lon = lineData[6]
+
+#print(ob_lon)
+
+
