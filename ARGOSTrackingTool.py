eadb1675c36616f1d2f51e15b3d5ac8677989d74
natblackford <natsblackford@gmail.com>
2023-10-02 12:09:27 -0400

    initial commit and adding front matter to ARGOS Tracking Script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,10 @@
+#------------------------------------------------------------------------------------
+# ARGOSTrackingTool.py
+# Description: This tool reads in raw ARGOS tracking data and allows users 
+#   to view coordinates for turtle location by inputting dates. It filters
+#   out lower quality ARGOS points. 
+#
+# Author: Nat Blackford (nsb32@duke.edu)
+# Date: 10/2/2023
+#------------------------------------------------------------------------------------
+
