a11dc0d07f61442c241ea68646e5513ea0fb97c9
natblackford <natsblackford@gmail.com>
2023-10-04 10:28:53 -0400

    changing from a for loop to a while loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -1,4 +1,5 @@
 #------------------------------------------------------------------------------------
+
 # ARGOSTrackingTool.py
 # Description: This tool reads in raw ARGOS tracking data and allows users 
 #   to view coordinates for turtle location by inputting dates. It filters
@@ -14,17 +15,16 @@ file_name = './data/raw/sara.txt'
 #Create a file object from the file
 file_object = open(file_name,'r')
 
-#Read contents of file into a list
-line_list = file_object.readlines()
+#Read the file as single lines
+lineString = file_object.readline()
 
-#Close the file
-file_object.close()
-
-#Pretend we read one line of data from the file
-for lineString in line_list:
+#Iterate through lines
+while lineString:
     #see if there is data
     if lineString[0] in  ("#","u"):
+        lineString = file_object.readline()
         continue #if no data, skip to next line
+    
     #Split the string into a list of data items
     lineData = lineString.split()
 
@@ -37,3 +37,9 @@ for lineString in line_list:
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
+    #skip to next line
+    lineString = file_object.readline()
+
+# Close the file
+file_object.close()
